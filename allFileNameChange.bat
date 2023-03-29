bdcce35e3db054c3cfed93d1465ebb4624351506
ffEuryale <68266676+ffEuryale@users.noreply.github.com>
2023-03-29 18:06:13 +0900

    v0.8
    다른 파티션에 있어도 실행 가능하게 변경

--- allFileNameChange.bat
+++ allFileNameChange.bat
@@ -1,5 +1,8 @@
 @echo off
 
+::확장자가 .bin일 경우 .BIN으로 변경
+ren *.bin *.BIN
+
 :: 이하의 파일이 없을 경우 스크립트 실행하지 않고 종료
 IF NOT EXIST %cd%\*.BIN EXIT
 IF NOT EXIST %cd%\*.xls EXIT
@@ -7,28 +10,30 @@ IF NOT EXIST %cd%\ROUTER\POINT EXIT
 IF NOT EXIST %cd%\DRILL\*.DRL EXIT
 IF NOT EXIST %cd%\DRILL\*.REP EXIT
 
-:: C:\의 php 디렉토리로 이하 파일 이동
-move %cd%\*.BIN C:\php\
-move %cd%\*.xls C:\php\
-move %cd%\ROUTER\POINT C:\php\
-move %cd%\DRILL\*.DRL C:\php\
-move %cd%\DRILL\*.REP C:\php\
+:: C:\의 php\@@WORK 디렉토리로 이하 파일 이동
+move %cd%\*.BIN C:\php\@@WORK
+move %cd%\*.xls C:\php\@@WORK
+move %cd%\ROUTER\POINT C:\php\@@WORK
+move %cd%\DRILL\*.DRL C:\php\@@WORK
+move %cd%\DRILL\*.REP C:\php\@@WORK
 
-:: PHP에서 xlsc.php 실행
-cd C:\php
+:: 실행 디렉토리가 E:\이므로 C:\로 이동 후 PHP에서 xlsc.php 실행
+cd C:\php\
+C:
 php.exe xlsc.php
 
 :: 원래 경로로 복귀
 pushd "%~dp0"
 
-:: C:\php 디렉토리에서 원래 경로로 다시 파일 이동
-move C:\php\*.BIN %cd%\
-move C:\php\*.xls %cd%\
-move C:\php\*.ROU %cd%\ROUTER\
-move C:\php\*.DRL %cd%\DRILL\
-move C:\php\*.REP %cd%\DRILL\
+:: C:\php\@@WORK 디렉토리에서 원래 경로로 다시 파일 이동
+move C:\php\@@WORK\*.BIN %cd%\
+move C:\php\@@WORK\*.xls %cd%\
+move C:\php\@@WORK\*.ROU %cd%\ROUTER\
+move C:\php\@@WORK\*.DRL %cd%\DRILL\
+move C:\php\@@WORK\*.REP %cd%\DRILL\
 
-:: DRILL, ROUTER 디렉토리에 작업지시서 엑셀 복사
+:: AOI, DRILL, ROUTER 디렉토리에 작업지시서 엑셀 복사
+copy %cd%\*.xls %cd%\AOI\
 copy %cd%\*.xls %cd%\DRILL\
 copy %cd%\*.xls %cd%\ROUTER\
 
--- xlsc.php
+++ xlsc.php
@@ -1,12 +1,12 @@
 <?php
 
-$file = glob("./*.BIN");
+$file = glob("./@@WORK/*.BIN");
 foreach($file as $key => $val){
 $fileName = pathinfo($val, PATHINFO_FILENAME);
 
-$changeName = rename("-작업지시서.xls", "$fileName-작업지시서.xls");
-$changeName = rename("POINT", "$fileName.ROU");
-$changeName = rename("drl.drl", "$fileName.DRL");
-$changeName = rename("1111.rep", "$fileName.REP");
+$changeName = rename("@@WORK/-작업지시서.xls", "@@WORK/$fileName-작업지시서.xls");
+$changeName = rename("@@WORK/POINT", "@@WORK/$fileName.ROU");
+$changeName = rename("@@WORK/drl.drl", "@@WORK/$fileName.DRL");
+$changeName = rename("@@WORK/1111.rep", "@@WORK/$fileName.REP");
 }
 ?>
